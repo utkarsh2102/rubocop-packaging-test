b66ef9f17ab891da76fcd41066116a703a407e90
Utkarsh Gupta <utkarsh@debian.org>
2020-06-09 18:39:22 +0530

    Initial commit

---
+++ lib/rubocop/packaging/version.rb
@@ -0,0 +1,5 @@
+module RuboCop
+  module Packaging
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/rubocop/packaging_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rubocop::Packaging do
+  it "has a version number" do
+    expect(Rubocop::Packaging::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
